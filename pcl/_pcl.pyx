335122c66826347f26003a9f39998a2c3d5ea04c
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-12-10 18:11:42 +0100

    Fix bug in buffer protocol support
    The view count was not correctly incremented beyond 1, so having two
    views and releasing one would bring it back to 0.

--- pcl/_pcl.pyx
+++ pcl/_pcl.pyx
@@ -151,7 +151,7 @@ cdef class SegmentationNormal:
         mpcl_sacnormal_set_axis(deref(self.me),ax,ay,az)
 
 
-# Empirically determine strides, for buffer support.
+# Empirically determine strides, for buffer protocol support.
 # XXX Is there a more elegant way to get these?
 cdef Py_ssize_t _strides[2]
 cdef BasePointCloud _pc_tmp = BasePointCloud(np.array([[1, 2, 3],
@@ -211,9 +211,9 @@ cdef class BasePointCloud:
         cdef Py_ssize_t npoints = self.thisptr().size()
 
         if self._view_count == 0:
-            self._view_count += 1
             self._shape[0] = npoints
             self._shape[1] = 3
+        self._view_count += 1
 
         buffer.buf = <char *>&(cpp.getptr_at(self.thisptr(), 0).x)
         buffer.format = 'f'
